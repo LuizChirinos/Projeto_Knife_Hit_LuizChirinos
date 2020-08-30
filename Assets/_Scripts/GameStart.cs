0b7e8e7980b7884ee0c5c3498c5b85685e47a8a3
LuizChirinos <luizmig.mch@gmail.com>
2020-08-30 18:49:33 -0300

    Fix Problemas de Resolucao da UI

---
+++ Assets/_Scripts/GameStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    private GameObject managerGO;
+    private ThrowKnife throwKnife;
+    private GameObject enemiesParent;
+    private Entity[] enemies;
+
+    private void Start()
+    {
+        enemiesParent = GameObject.Find("Enemies");
+        managerGO = GameObject.Find("Manager");
+        throwKnife = managerGO.GetComponent<ThrowKnife>();
+        enemies = enemiesParent.GetComponentsInChildren<Entity>();
+    }
+}
